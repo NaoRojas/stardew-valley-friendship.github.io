d86ad1202ab3fa35bc5109491ee3f2c102fd6d4c
naoreds <naorojash@gmail.com>
2024-10-27 01:35:52 -0600

    Refactor search functionality and UI styling

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,27 @@ import { useState } from 'react'
 function App() {
   const characters = data.characters
   const [search, setSearch] = useState('')
-  const [searchItem, setSearchItem] = useState('')
-
-  const filteredCharacters = characters.filter(
-    (character) =>
-      character.name.toLowerCase().includes(search.toLowerCase()) &&
-      (character.loves.some((love) =>
-        love.name.toLowerCase().includes(searchItem.toLowerCase())
-      ) ||
-        character.likes.some((like) =>
-          like.name.toLowerCase().includes(searchItem.toLowerCase())
-        ) ||
-        character.hates.some((dislike) =>
-          dislike.name.toLowerCase().includes(searchItem.toLowerCase())
-        ))
-  )
+  const [filterType, setFilterType] = useState('character')
+  const handleClear = () => {
+    setSearch('')
+  }
+  const filteredCharacters =
+    filterType === 'character'
+      ? characters.filter((character) =>
+          character.name.toLowerCase().includes(search.toLowerCase())
+        )
+      : characters.filter(
+          (character) =>
+            character.loves.some((love) =>
+              love.name.toLowerCase().includes(search.toLowerCase())
+            ) ||
+            character.likes.some((like) =>
+              like.name.toLowerCase().includes(search.toLowerCase())
+            ) ||
+            character.hates.some((hate) =>
+              hate.name.toLowerCase().includes(search.toLowerCase())
+            )
+        )
 
   return (
     <>
@@ -30,21 +36,27 @@ function App() {
         </span>
         <h1>Friendship</h1>
       </div>
-      <div className="flex-row center">
+      <div className="flex-row center search-container">
+        <select
+          value={filterType}
+          onChange={(e) => setFilterType(e.target.value)}
+        >
+          <option value="character">Character</option>
+          <option value="item">Item</option>
+        </select>
         <input
           type="text"
-          placeholder="Search Character"
+          placeholder="Search"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
-        <input
-          type="text"
-          placeholder="Search Item"
-          value={searchItem}
-          onChange={(e) => setSearchItem(e.target.value)}
-        />
+        {search && (
+          <button className="clear-button" onClick={handleClear}>
+            <i className="material-icons">clear</i>
+          </button>
+        )}
       </div>
-      <div className="flex-col center">
+      <div className="flex-col center characters">
         {filteredCharacters.map((character) => (
           <CharacterCard
             key={character.name}
